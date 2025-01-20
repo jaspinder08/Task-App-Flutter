a3fd7000afe9c9c41e1c8e4f6e30e0972091e5c6
jaspinder08 <jaspinderkaurjk08@gmail.com>
2025-01-20 15:06:01 +0530

    splash screen 01

---
+++ lib/register_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ReisterScreen extends StatefulWidget {
+  const ReisterScreen({super.key});
+
+  @override
+  State<ReisterScreen> createState() => _ReisterScreenState();
+}
+
+class _ReisterScreenState extends State<ReisterScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/utility/color_constants.dart
@@ -0,0 +1,9 @@
+// import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class Appcolors {
+  static const Color primaryColor = Color(0xFF002E99);
+  static const Color white = Colors.white;
+
+  static const Color black = Colors.black;
+}
